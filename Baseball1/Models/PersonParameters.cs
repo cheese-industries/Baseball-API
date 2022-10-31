6c9782ba81aff34a71a558e857ee207e76ca8d3b
Dan Lee <thejamootz@gmail.com>
2022-10-31 14:47:38 -0400

    Delete Baseball1 directory

--- Baseball1/Models/PersonParameters.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Baseball1.Models
-{
-    public class PersonParameters
-    {
-        public List<Person> Persons { get; set; } = new List<Person>();
-        public int Pages { get; set; }
-        public int CurrentPage { get; set; }
-    }
-}
